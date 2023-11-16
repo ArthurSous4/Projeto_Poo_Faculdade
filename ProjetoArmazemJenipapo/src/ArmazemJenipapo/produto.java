39107099d2993c6b1e61b45bf661914069cbcafe
Arthur Lucas <123699745+ArthurSous4@users.noreply.github.com>
2023-11-15 21:51:57 -0300

    Add files via upload

--- ProjetoArmazemJenipapo/src/ArmazemJenipapo/produto.java
+++ ProjetoArmazemJenipapo/src/ArmazemJenipapo/produto.java
@@ -1,3 +1,21 @@
 package ArmazemJenipapo;
 
+public class produto {
+    
+    public float id;
+    public float cod;
+    public String nome;
+    public String desc;
+    public String fornec;
+    public String dtcadast;
 
+    produto(float id, float cod, String nome, String desc, String fornec,String dtcadast){
+        this.id = id;
+        this.cod = cod;
+        this.nome = nome;
+        this.desc = desc;
+        this.fornec = fornec;
+        this.dtcadast = dtcadast;
+    }
+
+}
