fa596161828d95124b5448b47ef3a16dbe2ca6e9
Arthur Lucas <123699745+ArthurSous4@users.noreply.github.com>
2023-11-16 00:24:38 -0300

    Add files via upload

--- ProjetoArmazemJenipapo/src/ArmazemJenipapo/fornecedor.java
+++ ProjetoArmazemJenipapo/src/ArmazemJenipapo/fornecedor.java
@@ -6,6 +6,7 @@ public class fornecedor extends Pessoa {
 	
 	private int idFornecedor;
 	private String nome;
+	private String senha;
 	private int cnpj;
 	private String logradouro;
 	private String bairro;
@@ -18,11 +19,12 @@ public class fornecedor extends Pessoa {
 	private Date dataNascimento;
 	public produto produtoDistribuido;
 	
-fornecedor(int idFornecedor, String nome, int cnpj, String logradouro,String bairro, String cidade, String estado,
+fornecedor(int idFornecedor, String nome, String senha, int cnpj, String logradouro,String bairro, String cidade, String estado,
 		int cep, int telefone, String email, Date dataCadastro, Date dataNascimento, produto produtoDistribuido){
 	
 	this.idFornecedor = idFornecedor;
 	this.nome = nome;
+	this.senha = senha;
 	this.cnpj = cnpj;
 	this.logradouro = logradouro;
 	this.bairro = bairro;
@@ -35,7 +37,111 @@ fornecedor(int idFornecedor, String nome, int cnpj, String logradouro,String bai
 	this.dataNascimento = dataNascimento;
 	this.produtoDistribuido = produtoDistribuido;
 }
-	
+
+public int getIdFornecedor() {
+	return idFornecedor;
+}
+
+public void setIdFornecedor(int idFornecedor) {
+	this.idFornecedor = idFornecedor;
+}
+
+public String getNome() {
+	return nome;
+}
+
+public void setNome(String nome) {
+	this.nome = nome;
+}
+
+public int getCnpj() {
+	return cnpj;
+}
+
+public void setCnpj(int cnpj) {
+	this.cnpj = cnpj;
+}
+
+public String getLogradouro() {
+	return logradouro;
+}
+
+public void setLogradouro(String logradouro) {
+	this.logradouro = logradouro;
+}
+
+public String getBairro() {
+	return bairro;
+}
+
+public void setBairro(String bairro) {
+	this.bairro = bairro;
+}
+
+public String getCidade() {
+	return cidade;
+}
+
+public void setCidade(String cidade) {
+	this.cidade = cidade;
+}
+
+public String getEstado() {
+	return estado;
+}
+
+public void setEstado(String estado) {
+	this.estado = estado;
+}
+
+public int getCep() {
+	return cep;
+}
+
+public void setCep(int cep) {
+	this.cep = cep;
+}
+
+public int getTelefone() {
+	return telefone;
+}
+
+public void setTelefone(int telefone) {
+	this.telefone = telefone;
+}
+
+public String getEmail() {
+	return email;
+}
+
+public void setEmail(String email) {
+	this.email = email;
+}
+
+public Date getDataCadastro() {
+	return dataCadastro;
+}
+
+public void setDataCadastro(Date dataCadastro) {
+	this.dataCadastro = dataCadastro;
+}
+
+public Date getDataNascimento() {
+	return dataNascimento;
+}
+
+public void setDataNascimento(Date dataNascimento) {
+	this.dataNascimento = dataNascimento;
+}
+
+public produto getProdutoDistribuido() {
+	return produtoDistribuido;
+}
+
+public void setProdutoDistribuido(produto produtoDistribuido) {
+	this.produtoDistribuido = produtoDistribuido;
+}
+
 private void adicionarFornecidos() {
 	
 }
--- ProjetoArmazemJenipapo/src/ArmazemJenipapo/produto.java
+++ ProjetoArmazemJenipapo/src/ArmazemJenipapo/produto.java
@@ -1,21 +1,21 @@
 package ArmazemJenipapo;
 
+import java.util.Date;
+
 public class produto {
     
     public float id;
-    public float cod;
     public String nome;
-    public String desc;
-    public String fornec;
-    public String dtcadast;
+    public String descricao;
+    public fornecedor nomeFornecedor;
+    public Date dataCadastro;
 
-    produto(float id, float cod, String nome, String desc, String fornec,String dtcadast){
+    produto(float id, String nome, String descricao, fornecedor nomeFornecedor,Date dataCadastro){
         this.id = id;
-        this.cod = cod;
         this.nome = nome;
-        this.desc = desc;
-        this.fornec = fornec;
-        this.dtcadast = dtcadast;
+        this.descricao = descricao;
+        this.nomeFornecedor = nomeFornecedor;
+        this.dataCadastro = new Date();
     }
 
 }
