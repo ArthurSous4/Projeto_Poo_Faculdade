f9e2dc366e25778c2f412a4d34d723cbef14375b
Arthur Lucas <123699745+ArthurSous4@users.noreply.github.com>
2023-11-18 21:04:24 -0300

    Add files via upload

--- ProjetoArmazemJenipapo/src/ArmazemJenipapo/Estoque.java
+++ ProjetoArmazemJenipapo/src/ArmazemJenipapo/Estoque.java
@@ -1,5 +1,25 @@
 package ArmazemJenipapo;
 
 public class Estoque {
+    private Produto nomeProduto;
+    private int quantidade;
+
+public Estoque(Produto nomeProduto, int quantidade) {
+	this.nomeProduto = nomeProduto;
+	this.quantidade = quantidade;
+   }
+
+
+public Produto getNomeProduto() {return nomeProduto;}
+
+public void setNomeProduto(Produto nomeProduto) {this.nomeProduto = nomeProduto;}
+  
+
+
+public int getQuantidade() {return quantidade;}
+    
+public void setQuantidade(int quantidade) {this.quantidade = quantidade;}
 
 }
+
+
